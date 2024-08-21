b299b9cecc55350749fa718d3ef22149140b1b4b
R60D <Uraani@live.fi>
2024-08-22 02:46:14 +0300

    no message

--- Source/ScaleFactor/BlueprintFunctions.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "BlueprintFunctions.h"
-
-float UBlueprintFunctions::GetMass(UStaticMeshComponent* obj)
-{
-	return obj->GetMass();
-}
