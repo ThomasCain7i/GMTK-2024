ef7badea70567efcc75f8f8b8d25263b24df46be
R60D <Uraani@live.fi>
2024-08-22 00:51:37 +0300

    no message

---
+++ Source/ScaleFactor/BlueprintFunctions.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BlueprintFunctions.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCALEFACTOR_API UBlueprintFunctions : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	UFUNCTION(BlueprintCallable, Category = "Test")
+	static void test();
+
+};
