7b1068c2ecd25e6664ecb9ca1a2185e02d437ec1
R60D <Uraani@live.fi>
2024-08-29 16:41:35 +0300

    no message

---
+++ Plugins/Intermediate/Build/Win64/UnrealEditor/Inc/UltiAdditions/UHT/UltiAdditions.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeUltiAdditions_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_UltiAdditions;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_UltiAdditions()
+	{
+		if (!Z_Registration_Info_UPackage__Script_UltiAdditions.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/UltiAdditions",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xCAC902F2,
+				0xC0B69A18,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_UltiAdditions.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_UltiAdditions.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_UltiAdditions(Z_Construct_UPackage__Script_UltiAdditions, TEXT("/Script/UltiAdditions"), Z_Registration_Info_UPackage__Script_UltiAdditions, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xCAC902F2, 0xC0B69A18));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
